ed3816d6c0ead1339618812454d9d1fd73fd9fb9
Szucs Krisztian <szucs.krisztian@gmail.com>
2016-03-16 14:38:17 +0100

    Added init imports: do, mesos, marathon, chronos

--- epos/__init__.py
+++ epos/__init__.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import, division, print_function
 
 from contextlib import contextmanager
 
-
 @contextmanager
 def ignoring(*exceptions):
     try:
@@ -10,9 +9,15 @@ def ignoring(*exceptions):
     except exceptions:
         pass
 
+with ignoring(ImportError):
+    from dask_mesos import mesos
 
 with ignoring(ImportError):
     from .context import Lock, Persist
 
 with ignoring(ImportError):
     from .spark import spark
+
+from dask.imperative import do
+from .chronos import chronos
+from .marathon import marathon
