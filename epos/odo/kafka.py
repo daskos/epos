bec431bce2bd56de453f2b192dc9f3c70dc19ff0
Szucs Krisztian <szucs.krisztian@gmail.com>
2016-08-18 14:09:46 +0200

    Removed decorators for argument filtering for kafka producer and consumer

--- epos/odo/kafka.py
+++ epos/odo/kafka.py
@@ -4,9 +4,6 @@ import re
 import json
 import inspect
 
-from six import wraps
-from toolz import curry
-
 from collections import Iterator
 from odo import resource, append, convert
 
@@ -34,26 +31,6 @@ class Kafka(object):
         self.topic = self.client.topics[topic]
 
 
-
-@curry
-def filter_kwargs(fn, available_kwargs=()):
-    @wraps(fn)
-    def wrapper(*args, **kwargs):
-        d = {k: v for k, v in kwargs.items() if k in available_kwargs}
-        return fn(*args, **d)
-
-    return wrapper
-
-
-simple_consumer_args = inspect.getargspec(SimpleConsumer.__init__)[0]
-simple_consumer = filter_kwargs(
-    available_kwargs=simple_consumer_args+['dshape', 'loads', 'kafka'])
-
-producer_args = inspect.getargspec(Producer.__init__)[0]
-producer = filter_kwargs(
-    available_kwargs=producer_args+['dshape', 'dumps', 'kafka'])
-
-
 @resource.register('kafka://.*')
 def resource_kafka(uri, kafka=None, **kwargs):
     pattern = r'kafka://(?P<host>[\w.-]*)?(:(?P<port>\d+))?/(?P<topic>[^\/]+)$'
@@ -63,17 +40,19 @@ def resource_kafka(uri, kafka=None, **kwargs):
 
 
 @convert.register(Iterator, Kafka)
-@simple_consumer
 def kafka_to_iterator(dst, dshape=None, loads=json.loads, kafka=None, **kwargs):
+    args = inspect.getargspec(SimpleConsumer.__init__)[0]
+    kwargs = {k: v for k, v in kwargs.items() if k in args}
     consumer = dst.topic.get_simple_consumer(**kwargs)
     for message in consumer:
         yield loads(message.value)
 
 
 @append.register(Kafka, (list, Iterator))
-@producer
 def append_iterator_to_kafka(dst, src, dshape=None, dumps=json.dumps,
                              kafka=None, **kwargs):
+    args = inspect.getargspec(Producer.__init__)[0]
+    kwargs = {k: v for k, v in kwargs.items() if k in args}
     with dst.topic.get_producer(**kwargs) as producer:
         for item in src:
             producer.produce(dumps(item))
@@ -81,9 +60,10 @@ def append_iterator_to_kafka(dst, src, dshape=None, dumps=json.dumps,
 
 
 @append.register(Kafka, object)  # anything else
-@producer
 def append_object_to_kafka(dst, src, dshape=None, dumps=json.dumps,
                            kafka=None, **kwargs):
+    args = inspect.getargspec(Producer.__init__)[0]
+    kwargs = {k: v for k, v in kwargs.items() if k in args}
     with dst.topic.get_producer(**kwargs) as producer:
         producer.produce(dumps(src))
     return kafka
